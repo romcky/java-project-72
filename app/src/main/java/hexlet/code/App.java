e073d0e0d7e94f1a3290a44fa0aad3536dbbb527
Anton Romanovsky <romcky@gmail.com>
2024-04-20 17:46:20 +0300

    step-2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+//import io.javalin.rendering.template.JavalinJte;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            //config.fileRenderer(new JavalinJte());
+        });
+        app.get("/", ctx -> ctx.result("Hello, Wold!"));
+        return app;
     }
 }
