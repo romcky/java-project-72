9b6e8ad41ad2a8d7aecc518f86edf534b73def0d
Anton Romanovsky <romcky@gmail.com>
2024-05-28 10:37:06 +0300

    add createUrl test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -28,4 +28,35 @@ public class AppTest {
         });
     }
 
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get(NamedRoutes.urlsPath());
+            Assertions.assertEquals(response.code(), 200);
+            Assertions.assertTrue(response.body().string().contains("Список добавленных сайтов"));
+        });
+    }
+
+    @Test
+    public void testCreateUrlWrong() {
+        JavalinTest.test(app, (server, client) -> {
+            var createResponse = client.post(NamedRoutes.urlsPath(), "url=testcom");
+            Assertions.assertEquals(createResponse.code(), 200);
+            var showResponse = client.get(NamedRoutes.urlPath(1));
+            Assertions.assertEquals(showResponse.code(), 200);
+            Assertions.assertFalse(showResponse.body().string().contains("testcom"));
+        });
+    }
+
+    @Test
+    public void testCreateUrlRith() {
+        JavalinTest.test(app, (server, client) -> {
+            var createResponse = client.post(NamedRoutes.urlsPath(), "url=https://test.com");
+            Assertions.assertEquals(createResponse.code(), 200);
+            var showResponse = client.get(NamedRoutes.urlPath(1));
+            Assertions.assertEquals(showResponse.code(), 200);
+            Assertions.assertTrue(showResponse.body().string().contains("https://test.com"));
+        });
+    }
+
 }
